c6eacefbabde3955fc808f0e310a65569c109364
Romicha Parvin <romichaparvin35@gmail.com>
2026-08-26 09:52:31 +0600

    redux setup

---
+++ app/redux/api/authApi.ts
@@ -0,0 +1,30 @@
+import { baseApi } from "./baseApi";
+
+interface LoginRequest {
+  email: string;
+  password: string;
+}
+
+interface LoginResponse {
+  accessToken: string;
+  refreshToken: string;
+  user: {
+    id: string;
+    name: string;
+    email: string;
+  };
+}
+
+export const authApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    login: builder.mutation<LoginResponse, LoginRequest>({
+      query: (body) => ({
+        url: "/auth/login",
+        method: "POST",
+        body,
+      }),
+    }),
+  }),
+});
+
+export const { useLoginMutation } = authApi;
